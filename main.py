489017e777b91290185569dbffea89092f0a2356
rapidarray1211 <wangjeff1211@gmail.com>
2024-11-21 01:49:14 -0500

    fix code hangup by not closing engine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pgn_parser import load_pgn_file, get_move_sequence
 from board import initialize_board, make_move
-from stockfish import evaluate_move_stockfish
+from stockfish import evaluate_move_stockfish, close_engine
 
 def play_game_pgn(pgn_file):
     # Load games from the PGN file
@@ -14,8 +14,11 @@ def play_game_pgn(pgn_file):
         
         board = initialize_board()
         for move in move_sequence:
+            print(move)
             make_move(board, move)
             evaluate_move_stockfish(board)
+        close_engine()
+    
 
 if __name__ == "__main__":
     play_game_pgn("data/lichess_dataset_1.pgn")
--- stockfish.py
+++ stockfish.py
@@ -5,17 +5,19 @@ from typing import List, Tuple
 
 STOCKFISH_PATH = "stockfish-windows-x86-64-avx2.exe"
 stockfish_depth = 15
-stockfish_levels = [1, 5, 10, 20]
+level = 10
 engine = chess.engine.SimpleEngine.popen_uci(STOCKFISH_PATH)
 
 def evaluate_move_stockfish(board: chess.Board):
-    for level in stockfish_levels:
-        engine.configure({"Skill level": level})
-        print(f"Evaluating games at Stockfish level {level}...\n")
-        evaluation = engine.analyse(board, chess.engine.Limit(depth=10))
-        score = evaluation["score"].relative
+    engine.configure({"Skill level": level})
+    print(f"Evaluating games at Stockfish level {level}...\n")
+    evaluation = engine.analyse(board, chess.engine.Limit(depth=10))
+    score = evaluation["score"].relative
 
-        if score.is_mate():
-            print(f"Stockfish Level {level}: Mate in {score.mate()}")
-        else:
-            print(f"Stockfish Level {level}: Evaluation {score.score() / 100:.2f} (centipawns)\n")
+    if score.is_mate():
+        print(f"Stockfish Level {level}: Mate in {score.mate()}")
+    else:
+        print(f"Stockfish Level {level}: Evaluation {score.score() / 100:.2f} (centipawns)\n")
+    
+def close_engine():
+    engine.quit()
